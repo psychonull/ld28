ec1bd03f7e4eb1a475d703067068563dd9c95edf
Matias Arriola <matias.arriola@gmail.com>
2013-12-15 12:43:29 -0300

    person options refactor

--- client/game/entities/person.js
+++ client/game/entities/person.js
@@ -2,24 +2,12 @@
 module.exports = function(game, options){
   
   var person = game.entities.make();
-
-  var pos = (options && _.pick(options, "x", "y")) || { x: 0, y: 0 };
-
-  person.add("position", pos);
-  person.add("velocity", { x: 0, y: 0 });
-  person.add("speed", options && _.has(options,"speed") ? options.speed : mumps.settings.defaultPersonSpeed);
-  //person.add("target", { x: 0, y: 0 });
-  person.add("size", { width: 80, height: 120 });
-  
-  person.add("angle", options && _.has(options,"angle") ? options.angle : 0);
-  person.add("scale", { x: 1, y: 1 });
-  person.add("mass", 0.01);
-
-  if (options && options.boundary){
-    person.add("boundary", options.boundary);
-  }
-  else {
-    person.add("boundary", {
+  options = _.defaults(options || {}, {
+    x: 0,
+    y: 0,
+    speed: mumps.settings.defaultPersonSpeed,
+    angle: 0,
+    boundary: {
       min: {
         x: 0,
         y: 0
@@ -28,8 +16,22 @@ module.exports = function(game, options){
         x: mumps.settings.worldSize.w,
         y: mumps.settings.worldSize.h
       }
-    });  
-  }
+    }
+  });
+
+  var pos = _.pick(options, "x", "y");
+
+  person.add("position", pos);
+  person.add("velocity", { x: 0, y: 0 });
+  person.add("speed", options.speed);
+  //person.add("target", { x: 0, y: 0 });
+  person.add("size", { width: 80, height: 120 });
+  
+  person.add("angle", options.angle);
+  person.add("scale", { x: 1, y: 1 });
+  person.add("mass", 0.01);
+
+  person.add("boundary", options.boundary);
 
 /*
   person.add("display", {
@@ -66,14 +68,14 @@ module.exports = function(game, options){
 
   person.add("dynamic");
 
-  if (options && options.npc || !_.has(options,"npc")){ // npc by default
+  if (options.npc || !_.has(options,"npc")){ // npc = false wont add npc
     person.add('npc', _.defaults(options.npc || {}, { rest: mumps.settings.defaultNPCRest }));
   }
 
   person.add('person');
   person.add("zindex", 2);
 
-  if(options && options.contagionFocus){
+  if(options.contagionFocus){
     person.add("contagionFocus", options.contagionFocus);
   }
 
