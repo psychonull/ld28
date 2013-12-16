fdefceeeb89ae8114d6b9e8bfe1ded2ac33c8946
Matias Arriola <matias.arriola@gmail.com>
2013-12-16 01:37:04 -0300

    level 3

--- client/game/data/level3.js
+++ client/game/data/level3.js
@@ -72,8 +72,8 @@ module.exports = {
     type: "fence"
   },
   { // auto
-    x:382,
-    y:334,
+    x:600,
+    y:200,
     w:429,
     h:171,
     type: "car"
@@ -84,6 +84,13 @@ module.exports = {
     w:283,
     h:186,
     type: "trash"
+  },
+  { // tacho
+    x:600,
+    y:696,
+    w:200,
+    h:70,
+    type: "trash"
   }
   ],
 
@@ -92,8 +99,98 @@ module.exports = {
     x: 1800,
     y: 350,
     npc: false,
-    angle: 90
-  }],
+    angle: 90,
+    skin: 1,
+    inmune: true
+  },
+  {
+    x: 700,
+    y: 500,
+    npc: {rest: [1, 3]},
+    skin: 3,
+    inmune: true,
+    speed: 3
+  },
+  {
+    x: 400,
+    y: 700,
+    npc: {rest: [5, 8]},
+    skin: 2,
+    inmune: true,
+    speed: 10,
+    contagionFocus: {
+      radius: 130,
+      power: 1
+    }
+  },
+  {
+    x: 900,
+    y: 700,
+    npc: {rest: [5, 8]},
+    skin: 2,
+    inmune: true,
+    speed: 10,
+    contagionFocus: {
+      radius: 130,
+      power: 1
+    }
+  },
+  {
+    x: 1100,
+    y: 200,
+    npc: {rest: [1, 3]},
+    skin: 5,
+    inmune: true,
+    speed: 4,
+    boundary: {
+      min: {
+        x:1000,y:30
+      },
+      max: {
+        x:1600,y:800
+      }
+    },
+    contagionFocus: {
+      radius: 200,
+      power: 0.4
+    }
+  },
+  {
+    x: 1200,
+    y: 500,
+    npc: {rest: [1, 3]},
+    skin: 5,
+    inmune: true,
+    speed: 4,
+    boundary: {
+      min: {
+        x:1000,y:30
+      },
+      max: {
+        x:1600,y:800
+      }
+    }
+  },
+  {
+    x: 1700,
+    y: 200,
+    npc: {rest: [0.5, 6]},
+    skin: 5,
+    speed: 3,
+    boundary: {
+      min: {
+        x:1300,y:30
+      },
+      max: {
+        x:1800,y:300
+      }
+    },
+    contagionFocus: {
+      radius: 200,
+      power: 0.5
+    }
+  }
+  ],
 
   objetive : {
     x: 1850,
