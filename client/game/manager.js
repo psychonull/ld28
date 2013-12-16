489c7b11d720b479a3d2d71b178b262a4940d0d7
Pablo Novas <pjnovas@gmail.com>
2013-12-16 01:44:07 -0300

    updated html styles

--- client/game/manager.js
+++ client/game/manager.js
@@ -33,16 +33,17 @@ module.exports = {
   showChapterPresentation: function(nbr){
     var self = this;
     var chapters = {
-      "1": "Chapter 1: Revelations",
-      "2": "Chapter 2: Realization",
-      "3": "Chapter 3: Panic",
-      "4": "Chapter 4: Perseverance",
-      "5": "Chapter 5: Reunion"
+      "1": "Revelations",
+      "2": "Realization",
+      "3": "Panic",
+      "4": "Perseverance",
+      "5": "Reunion"
     };
     $("canvas").hide();
-    $("#chapter h1").text(chapters[nbr]);
+    $("#chapter h1").text("CHAPTER " + nbr);
+    $("#chapter h2").text(chapters[nbr]);
     $("#chapter").height($(window).height());
-    $("#chapter").width($(window).width());
+    $("#chapter, .main").width($(window).width());
     $("#chapter").show();
     $("#chapter a.continue").on("click", function(){
       $("canvas").show();
@@ -53,21 +54,21 @@ module.exports = {
 
   getCinemaHTML: function(level){
     var html = {
-      "1": ["Why the heck she is not here? What does this note mean?:", 
-          " <quote>Chimataru: I still don't understand why you keep doing always the same thing.",
-          " Plus these strange mumps keep propagating in this sh***y town. I don't feel like sitting",
-          " here for you. I went with the girls. </quote> Signed - lilita"].join(''),
-      "2": ["I felt strange for a bit. I'm starting to think that ",
-          " these mumps are not yet another media invention.",
-          "</br>If all of this is really happening, I want to be with her.",
-          "I know this is my last chance"].join(''),
-      "3": ["Chimataru: hey, why are you blocking the street?</br>",
-          "Unknown human: I won't let them come. The mumps are contagious and stronger in the other side </br>",
-          "Chimataru: I don't care. Goodbye sir"],
-      "4": ["The people is not being polite. Not at all. They keep pushing me. However, I feel ready for this."],
-      "5": ["Chimataru: I made impossible things to get to you.</br>",
-           "Lilita: I'm really sorry. I regret those silly discussions and life-wasting Angry moments.",
-           "Just let's enjoy. <b>You only get one life.</b>" ]
+      "1": ["<p>Why the heck she is not here? What does this note mean?:</p>", 
+          " <quote><p>Chimataru: I still don't understand why you keep doing always the same thing.</p>",
+          " <p>Plus these strange mumps keep propagating in this sh***y town. I don't feel like sitting",
+          " here for you. I went out with the girls into another bar. </p></quote> <p>Signed - lilita</p>"].join(''),
+      "2": ["<p>I felt strange for a bit. </p><p>I'm starting to think ",
+          " these mumps are not yet another media invention.</p>",
+          "<p>If all of this is really happening I want to be with her.</p>",
+          "<p>I know this is my last chance</p>"].join(''),
+      "3": ["<p><label>Chimataru:</label><span>hey, why are you blocking the street?</span></p>",
+          "<p><label>Unknown human:</label><span>I won't let them come. The mumps are contagious and stronger in the other side</span></p>",
+          "<p><label>Chimataru:</label><span>I don't care. Goodbye sir</span></p>"],
+      "4": ["<p>People is not being polite. Not at all. They keep pushing me.</p> <p>However, I feel ready for this.</p>"],
+      "5": ["<p><label>Chimataru:</label><span>I've made impossible things to get to you.</span></p>",
+           "<p><label>Lilita:</label><span>I'm really sorry. I regret those silly discussions and life-wasting angry moments.</span></p>",
+           "<p>Just let's enjoy. <b>You only get one life.</b></p>" ]
 
     };
 
